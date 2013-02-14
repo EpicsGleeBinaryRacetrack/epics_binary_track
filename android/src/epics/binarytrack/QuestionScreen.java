92ee3c0d79b3f6d4bc959f70afb145cb1f45050e
Jaye <jfranklin773@gmail.com>
2013-02-13 22:14:05 -0500

    init

---
+++ android/src/epics/binarytrack/QuestionScreen.java
@@ -0,0 +1,14 @@
+package epics.binarytrack;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class QuestionScreen extends Activity {
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.android_question_screen);
+	}
+
+}
