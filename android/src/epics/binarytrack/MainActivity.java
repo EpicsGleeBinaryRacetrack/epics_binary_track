86d08a4336374b27b9dda15c2de49343d8f49426
rahul0705 <rahul0705@gmail.com>
2013-02-24 07:08:35 -0500

    added login screen

--- android/src/epics/binarytrack/MainActivity.java
+++ android/src/epics/binarytrack/MainActivity.java
@@ -1,14 +1,26 @@
 package epics.binarytrack;
 
+import java.util.ArrayList;
+
 import android.app.Activity;
 import android.app.ProgressDialog;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.TextView.OnEditorActionListener;
 
 public class MainActivity extends Activity {
-
+	ArrayList<String> usernames = new ArrayList<String>();
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -23,7 +35,13 @@ public class MainActivity extends Activity {
 	}
 	
 	public void startButton(View v){
-
+		AutoCompleteTextView username = (AutoCompleteTextView)this.findViewById(R.id.usernames);
+		String input = username.getEditableText().toString();
+		if(usernames.indexOf(input) == -1)
+			usernames.add(input);
+		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_dropdown_item_1line, usernames);
+		username.setAdapter(adapter);
+		Toast.makeText(MainActivity.this, "Welcome " + input, Toast.LENGTH_SHORT).show();
 		this.startActivity(new Intent(this,QuestionScreen.class));
 	}
 
