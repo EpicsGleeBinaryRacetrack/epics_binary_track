b032a51222d3591678dd33a806a6b5841d4198bd
epics dummy <epics-glee@ecn.purdue.edu>
2013-04-14 16:58:46 -0400

    phone will now start app and raspberry pi will connect when startup.sh works

--- server/rahul_code_4_player1.sh
+++ server/rahul_code_4_player1.sh
@@ -1,1 +1,2 @@
 echo 'rahuls code for player 1 has been exectued'
+`/usr/bin/sudo ../gpioControls/moveCars 1`
--- server/rahul_code_4_player2.sh
+++ server/rahul_code_4_player2.sh
@@ -1,3 +1,3 @@
 
 echo 'rahul code for player 2 has been exectued'
-
+`/usr/bin/sudo ../gpioControls/moveCars 2`
