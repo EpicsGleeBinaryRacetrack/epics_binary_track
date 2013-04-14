14e321b3be9c8c4c2d482270539510c65401f18d
Jaye <jfranklin773@gmail.com>
2013-04-14 16:55:22 -0300

    Update startup.sh

--- server/startup.sh
+++ server/startup.sh
@@ -1,4 +1,3 @@
-
 adb -s c0890b1707e4c65f forward tcp:5001 tcp:8081
 adb -s c089052a0120707f forward tcp:5002 tcp:8081
 
@@ -8,3 +7,5 @@ adb -s c089052a0120707f shell am start -n epics.binarytrack/epics.binarytrack.Ma
 
 adb -s c0890b1707e4c65f shell am force-stop epics.binarytrack
 adb -s c0890b1707e4c65f shell am start -n epics.binarytrack/epics.binarytrack.MainActivity
+
+java Main
