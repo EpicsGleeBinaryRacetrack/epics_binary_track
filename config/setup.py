a99fee8705afe6663cc30134afeee660080e2783
Timothy McPherson <tmcphers@purdue.edu>
2013-04-16 13:43:39 -0400

    Complete setup.py.  Still needs documentation.

--- config/setup.py
+++ config/setup.py
@@ -1,9 +1,42 @@
 
 from cx_Freeze import setup, Executable
+import sys
+
+base = None
+
+print("foobar")
+print(sys.platform)
+
+if sys.platform == "win32":
+	base = "WIN32GUI"
 
 setup(
 	name = "Binary Track Configuration Editor",
 	version = "1.0",
 	description = "Configuration editor for the GLEE Binary Racetrack",
-	executables = [Executable("glee_config.pyw", base="WIN32GUI")]
-	)
+	executables = [Executable(
+        "glee_config.pyw", 
+        base=base,
+        #base="WIN32GUI",
+        compress=True,
+        copyDependentFiles = True
+        #appendScriptToExe = True
+        
+    )],
+    options = {
+        "build_exe":{
+            "includes" : [
+                "tkinter.tix",
+                "tkinter",
+                "tkinter.ttk"
+                #"tkinter.tix"
+                ],
+            "packages" : [
+                "tkinter.tix"
+                ],
+            "include_files" : [
+                "questions.txt"
+            ]
+        }
+    }
+)
