075e3cdb14d62fad85d7f4a1e7f811c15fa10e13
epics dummy <epics-glee@ecn.purdue.edu>
2013-04-16 19:10:14 -0400

    added all adb stuff and working code now

--- server/run.sh
+++ server/run.sh
@@ -1,8 +1,6 @@
-/usr/bin/sudo mount -o uid=pi,gid=pi /dev/sda1 /mnt/USB
+#!/bin/bash
 
-/usr/bin/sudo ./adb -s c089052a0120707f push /mnt/USB/build/exe.win-amd64-3.3/questions.txt /mnt/sdcard/questions.txt
-/usr/bin/sudo ./adb -s c0890b1707e4c65f push /mnt/USB/build/exe.win-amd64-3.3/questions.txt /mnt/sdcard/questions.txt
+/usr/bin/sudo /home/pi/epics_binary_track/server/adb -s c089052a0120707f push /media/usb0/build/exe.win-amd64-3.3/questions.txt /mnt/sdcard/questions.txt
+/usr/bin/sudo /home/pi/epics_binary_track/server/adb -s c0890b1707e4c65f push /media/usb0/build/exe.win-amd64-3.3/questions.txt /mnt/sdcard/questions.txt
 
-sudo ./startup.sh
-
-/usr/bin/sudo umount /mnt/USB
+/usr/bin/sudo /home/pi/epics_binary_track/server/startup.sh
