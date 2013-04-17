fb33f16d922d147e6ab3fd4e4ed5e9e6bda20b43
Jaye <jfranklin773@gmail.com>
2013-04-17 17:10:33 -0400

    fixed some software errors

--- android/src/epics/binarytrack/fragments/MultiChoiceQuestionFragment.java
+++ android/src/epics/binarytrack/fragments/MultiChoiceQuestionFragment.java
@@ -1,6 +1,7 @@
 package epics.binarytrack.fragments;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Iterator;
 import java.util.Set;
 
@@ -14,14 +15,22 @@ import android.widget.Button;
 import android.widget.TextView;
 import epics.binarytrack.R;
 import epics.binarytrack.questions.MultiChoiceQuestion;
+import epics.binarytrack.questions.Question;
 
 public class MultiChoiceQuestionFragment extends QuestionFragment {
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
-		View layout = inflater.inflate(R.layout.choice_4_question, container,
-				false);
+		View layout = null;
+		if(mQuestion.getType()==Question.MULTI_CHOICE_4){
+			layout=inflater.inflate(R.layout.choice_4_question, container,
+						false);
+		}else{
+			layout=inflater.inflate(R.layout.choice_2_question, container,
+					false);
+		}
+
 		TextView text = (TextView) layout.findViewById(R.id.textView1);
 		OnClickListener listener = new OnClickListener() {
 			@Override
@@ -41,6 +50,7 @@ public class MultiChoiceQuestionFragment extends QuestionFragment {
 
 		Button b = (Button) layout.findViewById(R.id.button1);
 		b.setOnClickListener(listener);
+		Collections.shuffle(answers);
 		String k =answers.get(0); 
 		b.setText(k);
 		Log.d("question",k);	
