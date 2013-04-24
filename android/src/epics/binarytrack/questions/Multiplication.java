1c82b3df03e8244a377e0265920972a74933e393
Jaye <jfranklin773@gmail.com>
2013-04-24 16:08:12 -0400

    added default category checking

--- android/src/epics/binarytrack/questions/Multiplication.java
+++ android/src/epics/binarytrack/questions/Multiplication.java
@@ -11,7 +11,7 @@ public class Multiplication extends Question
 		int num1 = new Random().nextInt(15);
 		int num2 = new Random().nextInt(15);
 		
-		setQuestion("Subtract: " + num1 + " * "+ Integer.toString(num2) + ".");
+		setQuestion("Multiplication: " + num1 + " * "+ num2 + ".");
 		int answer = num1 * num2;
 		setAnswer(answer+"");		
 	}
