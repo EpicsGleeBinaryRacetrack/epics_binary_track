89056dfed3b621cbbc80a8d8c0da935f57db20a7
Timothy McPherson <tmcphers@purdue.edu>
2013-04-26 15:10:39 -0400

    Added some documentation to setup.py

--- config/setup.py
+++ config/setup.py
@@ -1,9 +1,20 @@
 
+# Usage:
+# python setup.py build
+# This will call this build script and use it to set all appropriate 
+# variables and compile it.  The build script will pull all 
+# needed files and libraries from their locations and store them
+# in one folder under build/.  For some reason, cx_Freeze ignores
+# Tkinter's Tix library, so that has to be copied manually.
+# Once it is copied manually, it works fine.  
+# Make sure questions.txt is included or this build will fail.
+
 from cx_Freeze import setup, Executable
 import sys
 
 base = None
 
+# Make it target GUI.
 if sys.platform == "win32":
 	base = "WIN32GUI"
 
@@ -14,10 +25,8 @@ setup(
 	executables = [Executable(
         "glee_config.pyw", 
         base=base,
-        #base="WIN32GUI",
         compress=True,
         copyDependentFiles = True
-        #appendScriptToExe = True
         
     )],
     options = {
@@ -31,6 +40,10 @@ setup(
             "packages" : [
                 "tkinter.tix"
                 ],
+            # Include questions.txt.  If questions.txt
+            # is not in the same folder as setup.py, 
+            # the b uild will fail.  Ensure it is in 
+            # the folder before attempting to build.
             "include_files" : [
                 "questions.txt"
             ]
